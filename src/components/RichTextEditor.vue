d0f833676d5726aa163d29816988ef4615a81ae7
LanceHE6 <2765543491@qq.com>
2023-12-08 22:51:17 +0800

    feat.新增富文本编辑器组件

---
+++ src/components/RichTextEditor.vue
@@ -0,0 +1,85 @@
+<template>
+  <div style="border: 1px solid #ccc; margin-top: 10px">
+    <Toolbar
+        :editor="editorRef"
+        :defaultConfig="toolbarConfig"
+        :mode="mode"
+        style="border-bottom: 1px solid #ccc"
+    />
+    <Editor
+        :defaultConfig="editorConfig"
+        :mode="mode"
+        v-model="valueHtml"
+        style="height: 400px; overflow-y: hidden"
+        @onCreated="handleCreated"
+        @onChange="handleChange"
+
+    />
+  </div>
+</template>
+
+<script setup>
+import '@wangeditor/editor/dist/css/style.css';
+import {Editor, Toolbar} from "@wangeditor/editor-for-vue";
+import {inject, onBeforeUnmount, onMounted, ref, shallowRef} from "vue";
+
+const serverUrl = inject("server_url")
+// 编辑器实例，必须用 shallowRef，重要！
+const editorRef = shallowRef();
+const toolbarConfig = {};
+const editorConfig = { placeholder: '请输入内容...' };
+editorConfig.MENU_CONF = {}
+// 指定图片上传的服务端地址
+editorConfig.MENU_CONF['uploadImage'] = {
+  server: serverUrl + '/upload/image',
+  fieldName: 'file',
+  // 小于该值就插入 base64 格式（而不上传），默认为 0
+  base64LimitSize: 5 * 1024 // 5kb
+}
+
+
+const mode = ref("default")
+
+// 编辑器内容
+const valueHtml = ref("")
+
+const props = defineProps({
+  modelValue:{
+    type: String,
+    default: ''
+  }
+})
+// 定义emit事件外抛数据
+const emit = defineEmits(["update:modelValue"])
+let initFinished = false
+
+
+// 组件销毁时，也及时销毁编辑器，重要！
+onBeforeUnmount(() => {
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.destroy();
+});
+onMounted(() =>{
+  setTimeout(() =>{
+    //设置延时，在外抛数据前将父级页面的数据读进来
+    valueHtml.value = props.modelValue
+    initFinished = true
+  }, 200)
+})
+// 编辑器回调函数
+const handleCreated = (editor) => {
+  console.log('created', editor);
+  editorRef.value = editor; // 记录 editor 实例，重要！
+};
+// 监听编辑器内容变化将编辑器内容外抛到父级页面中
+const handleChange = (editor) => {
+  if  (initFinished) {
+    emit('update:modelValue', valueHtml.value)
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
