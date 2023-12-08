227e4cd06d8278744a514ec6a0350a51e4a3c86b
LanceHE6 <2765543491@qq.com>
2023-12-08 22:53:07 +0800

    feat.新增富文本编辑器

--- src/views/dashboard/Article.vue
+++ src/views/dashboard/Article.vue
@@ -1,8 +1,49 @@
 <template>
+  <n-card style="margin-bottom: 16px">
+    <n-tabs default-value="oasis" justify-content="start" type="line">
+      <n-tab-pane name="oasis" tab="Oasis">
+        Wonderwall
+      </n-tab-pane>
+      <n-tab-pane name="add" tab="添加文章">
+        <n-form>
 
+          <n-form-item label="标题">
+            <n-input v-model:value="addArticle.title" placeholder="请输入标题"/>
+          </n-form-item>
+          <n-form-item label="内容">
+            <rich-text-editor v-model="addArticle.content"></rich-text-editor>
+          </n-form-item>
+
+        </n-form>
+      </n-tab-pane>
+      <n-tab-pane name="jay chou" tab="周杰伦">
+        七里香
+      </n-tab-pane>
+    </n-tabs>
+  </n-card>
+
+  {{ addArticle.content }}
 </template>
 
 <script setup>
+import {ref, reactive, inject, onMounted} from "vue";
+import {UserStore} from "@/store/UserStore";
+import {useRouter} from "vue-router";
+import RichTextEditor from "@/components/RichTextEditor.vue";
+
+//注入获取axios
+const axios = inject("axios")
+
+const userStore = UserStore()
+const router = useRouter()
+const message = inject("message")
+const dialog = inject("dialog")
+
+const addArticle = reactive({
+  title: "",
+  content: "",
+  categoryId: "",
+})
 
 </script>
 
