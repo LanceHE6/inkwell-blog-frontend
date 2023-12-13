9b79084bac2d286aa22a2ba3f5c1ff76b94f439b
LanceHE6 <2765543491@qq.com>
2023-12-14 07:55:13 +0800

    feat.增加用户信息界面

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="user-panel">
+    <n-card>
+      <n-space vertical class="container">
+        <n-avatar
+            round
+            :size="100"
+            :src=loadAvatar
+        />
+        <div>昵称： {{userStore.nickname}}</div>
+        <div>UID： {{userStore.uid}}</div>
+      </n-space>
+
+    </n-card>
+  </div>
+
+</template>
+
+<script setup>
+import {UserStore} from "@/store/UserStore";
+import {computed, onMounted} from "vue";
+
+const userStore = UserStore()
+
+onMounted(() =>{
+
+})
+
+const loadAvatar = computed(() => {
+  if (userStore.avatar === ""){
+    return "https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"
+  }
+  return userStore.avatar
+})
+</script>
+
+<style scoped>
+.user-panel{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 500px;
+}
+.container{
+  position: fixed;
+  top: 50%;
+  left: 50%;
+}
+</style>
