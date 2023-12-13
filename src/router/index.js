11bb830df5f93ef54df38a3d3a7369d42b35dffa
LanceHE6 <2765543491@qq.com>
2023-12-14 07:57:35 +0800

    feat.增加文章详情页和用户信息路由，取消管理员信息二级路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,23 @@ import Signup from "@/views/Signup.vue";
 import Dashboard from "@/views/dashboard/Dashboard.vue";
 import Category from "@/views/dashboard/Category.vue";
 import Article from "@/views/dashboard/Article.vue";
-import AdminInfo from "@/views/dashboard/AdminInfo.vue";
-// import HomePage from "@/views/HomePage.vue";
+import Home from "@/views/Home.vue";
+import ArticleDetail from "@/views/ArticleDetail.vue";
+import UserInfo from "@/views/UserInfo.vue";
+
 
 
 const routes = [
-    // { path: '/', component: HomePage , name: 'homepage'},
-    // { path: '/home', component: HomePage , name: 'home'},
+    { path: '/', component: Home , name: 'homepage'},
+    { path: '/home', component: Home , name: 'home'},
     { path: '/login', component: Login , name: 'login'},
     { path: '/signup', component: Signup , name: 'signup'},
+    { path: '/detail', component: ArticleDetail , name: 'articleDetail'},
+    { path: '/userInfo', component: UserInfo , name: 'userInfo'},
     { path: '/dashboard', component: Dashboard , name: 'dashboard', children: [
             // 二级路由
             { path: '/dashboard/category', component: Category , name: 'category'},
             { path: '/dashboard/article', component: Article , name: 'article'},
-            { path: '/dashboard/adminInfo', component: AdminInfo , name: 'adminInfo'},
         ]},
 
 ];
